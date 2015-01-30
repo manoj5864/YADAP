436154d36cce978e0ab8d898a09d45f2e5635dad
Manoj <manoj5864@gmail.com>
2015-01-30 15:36:59 +0100

    minor change

--- app/Global.java
+++ app/Global.java
@@ -69,8 +69,7 @@ public class Global extends GlobalSettings {
 
 	private void initialData() {
 		if (SecurityRole.find.findRowCount() == 0) {
-			for (final String roleName : Arrays
-					.asList(controllers.Application.USER_ROLE)) {
+			for (final String roleName : Arrays.asList(controllers.Application.USER_ROLE)) {
 				final SecurityRole role = new SecurityRole();
 				role.roleName = roleName;
 				role.save();
